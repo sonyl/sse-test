f0b695c679fa751c8bf528d7d319cbcc9e3a4e57
Gerhard Dickescheid <gdd@eacg.de>
2016-08-25 15:12:34 +0200

    added periodic connection cleanup, to fix CLOSE_WAIT issue

--- src/main/java/de/eacg/poc/controller/SlaEventUiListener.java
+++ src/main/java/de/eacg/poc/controller/SlaEventUiListener.java
@@ -3,9 +3,11 @@ package de.eacg.poc.controller;
 import org.glassfish.jersey.media.sse.EventOutput;
 import org.glassfish.jersey.media.sse.OutboundEvent;
 import org.glassfish.jersey.media.sse.SseBroadcaster;
+import org.glassfish.jersey.server.BroadcasterListener;
 import org.glassfish.jersey.server.ChunkedOutput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PreDestroy;
@@ -18,18 +20,7 @@ public class SlaEventUiListener {
 
     private static final Logger logger = LoggerFactory.getLogger(SlaEventUiListener.class);
 
-    private SseBroadcaster sseBroadcaster = new SseBroadcaster() {
-        @Override
-        public void onException(ChunkedOutput<OutboundEvent> chunkedOutput, Exception exception) {
-//            try {
-                logger.error("Failed to handle message, closing and removing ChunkedOutput");
-//                chunkedOutput.close();
-//                this.remove(chunkedOutput);
-//            } catch (IOException e) {
-//                logger.error("Failed to cleanup SSE", e);
-//            }
-        }
-    };
+    private SseBroadcaster sseBroadcaster = new SseBroadcaster();
 
 
     public void postMessage(String message) {
@@ -58,5 +49,36 @@ public class SlaEventUiListener {
     public void destroy() throws IOException {
         sseBroadcaster.closeAll();
         logger.debug("SlaEventUiListener DESTROYED");
+    }
+
+
+    @Scheduled(fixedDelay=60000)
+    public void connectionReaper() {
+        logger.debug("connectionReaper called, cleanup SSE connections");
+        try {
+            final OutboundEvent event = new OutboundEvent.Builder()
+                    .name("cleanup")
+                    .comment("")
+                    .build();
+            final int[] statistics = new int[] {0, 0};
+            final BroadcasterListener<OutboundEvent> listener = new BroadcasterListener<OutboundEvent>() {
+                @Override
+                public void onClose(ChunkedOutput<OutboundEvent> chunkedOutput) {
+                    statistics[0]++;
+                }
+                @Override
+                public void onException(ChunkedOutput<OutboundEvent> chunkedOutput, Exception exception) {
+                    statistics[1]++;
+                }
+
+            };
+            sseBroadcaster.add(listener);
+            sseBroadcaster.broadcast(event);
+            sseBroadcaster.remove(listener);
+            logger.debug("SSE connection cleanup finished: connections closed: {}, exceptions {}", statistics[0], statistics[1]);
+
+        } catch (Exception e) {
+            logger.error("Failed to broadcast event", e);
+        }
     }
 }
